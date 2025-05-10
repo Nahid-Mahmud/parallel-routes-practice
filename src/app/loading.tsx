c1a6c986a7f83a1a43d9860c6c5c828cee5fe871
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-05-10 23:00:45 +0600

    Refactor loading component to enhance visual feedback with animated dots and improved styling

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,18 @@
 const Loading = () => {
   return (
     <div>
-      <div className="h-full w-full flex items-center justify-center text-3xl font-bold border-2 border-gray-300 rounded-lg animate-pulse">
-        Loading Main Page...
+      <div className="h-screen text-white bg-black w-full flex items-center justify-center text-3xl font-bold border-2 border-gray-300 rounded-lg ">
+        Loading Main Page{" "}
+        {[".", "..", "..."].map((dot, index) => (
+          <span
+            key={index}
+            className={`animate-pulse ${
+              index === 0 ? "text-red-500" : index === 1 ? "text-green-500" : "text-blue-500"
+            }`}
+          >
+            {dot}
+          </span>
+        ))}{" "}
       </div>
     </div>
   );
